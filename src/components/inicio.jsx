03f0a35e06d654f1e6e96694ce9e0e846b785bbe
WesitosFsa <137083225+WesitosFsa@users.noreply.github.com>
2024-03-04 15:07:08 -0500

    Añadiendo archivos

---
+++ src/components/inicio.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+const LoginPage = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log('Email:', email, 'Password:', password);
+    // Aquí puedes agregar la lógica para iniciar sesión
+  };
+
+  return (
+    <div className="container">
+      <h1>Bienvenido</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" required />
+
+        <label htmlFor="password">Contraseña:</label>
+        <input type="password" id="password" name="password" required />
+
+        <input type="checkbox" id="remember" />
+        <label htmlFor="remember">Recuérdame</label>
+
+        <button type="submit">Entrar</button>
+      </form>
+      <br />
+      <button className="volver" onClick={() => window.location.href = '/'}>Pagina Principal</button>
+      <br />
+      <button className="proyectos" onClick={() => window.location.href = '/Proyectos'}>Pagina de Proyectos</button>
+      <br />
+      <button className="proyectos" onClick={() => window.location.href = '/administrador'}>Pagina del administrador</button>
+      <br />
+      <div className="social-login">
+        <button className="google-login">Entrar con Google</button>
+        <button className="microsoft-login">Entrar con Microsoft</button>
+      </div>
+      <br />
+      <a href="#">Olvidé mi contraseña</a>
+    </div>
+  );
+};
+export default LoginPage;
