94d495a421ccac750f06c231c1c901ce774b7723
WesitosFsa <137083225+WesitosFsa@users.noreply.github.com>
2024-03-04 15:10:41 -0500

    Navegacion

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './inicio.css';
+import './App.css';
 import LoginPage from './components/inicio';
-
+import MainPage from './components/main'; // Importa el componente de la página principal
+import ProyectosPage from './components/proyectos'; // Importa el componente de la página de proyectos
+import AdminPage from './components/administrador'; // Importa el componente de la página del administrador
 
 function App() {
   return (
@@ -11,12 +13,16 @@ function App() {
         <header className="App-header">
         </header>
         <Routes>
-  
+          <Route path="/" element={<MainPage />} /> {/* Página principal */}
           <Route path="/inicio" element={<LoginPage />} /> {/* Página de inicio de sesión */}
+          <Route path="/proyectos" element={<ProyectosPage />} /> {/* Página de proyectos */}
+          <Route path="/administrador" element={<AdminPage />} /> {/* Página de administrador */}
         </Routes>
       </div>
     </Router>
   );
 }
+
+export default App;
 
 export default App;
